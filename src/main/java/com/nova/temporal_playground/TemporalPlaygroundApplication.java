520580973601db2fb9437221e51d5456bab77f52
Sharath Balaraj <sbalaraj@onetrust.com>
2026-03-16 18:06:23 +0530

    Initial local project setup

---
+++ src/main/java/com/nova/temporal_playground/TemporalPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.nova.temporal_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalPlaygroundApplication.class, args);
+	}
+
+}
